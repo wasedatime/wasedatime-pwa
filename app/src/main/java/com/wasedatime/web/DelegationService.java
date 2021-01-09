0f93477e999a32c538de8c44083a3791a90ad573
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-01-10 04:57:20 +0900

    add project files

---
+++ app/src/main/java/com/wasedatime/web/DelegationService.java
@@ -0,0 +1,14 @@
+package com.wasedatime.web;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
